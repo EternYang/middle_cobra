8084153e0adb7d36bb0fe57ef7b97c4f38aa7bd1
EternYang <y312193268@gmail.com>
2018-08-02 18:37:59 +0800

    setting the support of email

--- api/api/settings.py
+++ api/api/settings.py
@@ -302,7 +302,7 @@ BLOCKED_IPS = ('111.231.105.60', '111.231.215.222', '198.23.206.98',)
 
 # 添加邮箱相关配置(the setting of email service)
 EMAIL_HOST = "smtp.gmail.com"   # 服务器
-EMAIL_PORT = 465                 # 一般情况下都为25
+EMAIL_PORT = 25                 # 一般情况下都为25
 EMAIL_HOST_USER = "support@itea.sg"   # 账号
 EMAIL_HOST_PASSWORD = "yoda1688"  # 密码
 EMAIL_USE_TLS = False             # 一般都为False
